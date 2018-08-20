e2b9459ca8a7f75e1ac70468bf580f3f9f6409cc
Anna <karlovskaya@gmail.com>
2018-08-20 10:31:55 -0700

    add style to AllSpeaker and Speaker components

---
+++ src/components/AllSpeakers/AllSpeakers.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SingleSpeaker from '../Speaker/Speaker';
+import speakersData from '../../speakers-data';
+import './allSpeakers.css';
+
+class AllSpeakers extends React.Component {
+  render() {
+    const { match } = this.props;
+    const username = match.params.username;
+
+    return (
+        <div className="container">
+            {
+                username ? <SingleSpeaker username={username} /> : <ListOfSpeakers />
+            }
+        </div>
+    )
+  }
+}
+
+export default AllSpeakers;
+
+const ListOfSpeakers = () => {
+    return (
+        <ul className="allSpeakers">
+            {
+                speakersData.map(speaker => (
+                    <li key={speaker.id} className="allSpeakers__li card">
+                        <div className="card-body">
+                            <div className="allSpeakers__title">
+                                <div className="title__img">
+                                    <img src={speaker.picture.large} alt={speaker.name.first}/>
+                                </div>
+                                <div>
+                                    <h3>{speaker.name.first} {speaker.name.last}</h3>
+                                    <p>Company:  <span className="text-danger">{speaker.company}</span></p>
+                                    <p className="card-subtitle mb-2 text-muted">{speaker.email}</p>
+                                </div>
+                            </div>
+
+                            <div className="allSpeakers__info">
+                                <h4>Topics</h4>
+                                <ul>
+                                    <li>{speaker.topic.first}</li>
+                                    <li>{speaker.topic.second}</li>
+                                </ul>
+                                <Link to={`/speakers/${speaker.login.username}`} className="card-link">Learn more</Link>
+                            </div>
+                        </div>
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
--- src/components/Speaker/Speaker.js
+++ src/components/Speaker/Speaker.js
@@ -3,29 +3,32 @@ import { Link } from 'react-router-dom';
 import './speaker.css';
 import speakersData from '../../speakers-data';
 
-const SingleSpeaker = (username) => {
+const SingleSpeaker = (username, props) => {
     // return array with matched username speaker
     const oneSpeaker = speakersData.filter(speaker => speaker.login.username === username.username);
     const socials = oneSpeaker[0].socials;
-    console.log('socials obj ', socials);
     const {twitter, linkedin, github} = socials;
-    console.log(twitter, linkedin, github);
 
     return (
-        <div>
-            <img src={oneSpeaker[0].picture.large} alt={oneSpeaker[0].name.first}/>
-            <h3>{oneSpeaker[0].name.first} {oneSpeaker[0].name.last}</h3>
-            <p>Company:  <span class="text-danger">{oneSpeaker[0].company}</span></p>
-            <p class="text-primary">{oneSpeaker[0].location.country}, {oneSpeaker[0].location.city}</p>
-            <p className="card-subtitle mb-2 text-muted">{oneSpeaker[0].email}</p>
-            <ul>
-                <li><a href={twitter} className="twitter" target="_blank">twitter</a></li>
-                <li><a href={linkedin} className="linkedin" target="_blank">linkedin</a></li>
-                <li><a href={github} className="github" target="_blank">github</a></li>
-            </ul>
-
-            <p>{oneSpeaker[0].description}</p>
-        </div>
+        <section className="speaker">
+            <div className="speaker__info">
+                <div className="speaker__img-wrapper">
+                    <img src={oneSpeaker[0].picture.large} alt={oneSpeaker[0].name.first}/>
+                </div>
+                <div className="speaker__title-wrapper">
+                    <h3>{oneSpeaker[0].name.first} {oneSpeaker[0].name.last}</h3>
+                    <p>Company:  <span className="text-danger">{oneSpeaker[0].company}</span></p>
+                    <p className="text-primary">{oneSpeaker[0].location.country}, {oneSpeaker[0].location.city}</p>
+                    <p className="card-subtitle mb-2 text-muted">{oneSpeaker[0].email}</p>
+                    <ul className="title-wrapper__socials">
+                        <li><a href={twitter} className="twitter" target="_blank">twitter</a></li>
+                        <li><a href={linkedin} className="linkedin" target="_blank">linkedin</a></li>
+                        <li><a href={github} className="github" target="_blank">github</a></li>
+                    </ul>
+                </div>
+            </div>
+            <p className="speaker__desctiption">{oneSpeaker[0].description}</p>
+        </section>
     )
 }
 
