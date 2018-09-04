3a5156523279fc88b81e6a8375c487369b9d3755
Anna <karlovskaya@gmail.com>
2018-09-04 10:51:35 -0700

    update search feature and some style

--- src/components/AllSpeakers/AllSpeakers.js
+++ src/components/AllSpeakers/AllSpeakers.js
@@ -50,11 +50,15 @@ class ListOfSpeakers extends React.Component {
             <div className="allSpeakers__wrapper">
                 <main>
                     <h1>Our Speakers</h1>
-
-                    <div className="form-group">
-                        <label>Search by name</label>
-                        <input type="text" className="form-control" onChange={this.handleSearch.bind(this)} />
-                    </div>
+                    <form className="form-group my-2 my-lg-0">
+                        <fieldset>
+                            <legend>Search by name</legend>
+                            <div className="input-wrapper">
+                                <input onChange={this.handleSearch.bind(this)} className="form-control mr-sm-2" type="text" placeholder="Search" />
+                                <i><img src="images/search-icon.png" alt="Search Icon"/></i>
+                            </div>
+                        </fieldset>
+                    </form>
                     <ul className="allSpeakers">
                         {
                             speakers.map(speaker => {
@@ -91,7 +95,7 @@ const Speaker = (props) => {
                         <li>{speaker.topic.first}</li>
                         <li>{speaker.topic.second}</li>
                     </ul>
-                    <Link to={`/speakers/${speaker.login.username}`} className="card-link">Learn more</Link>
+                    <Link to={`/speakers/${speaker.login.username}`} className="btn btn-primary btn-sm">Learn more</Link>
                 </div>
             </div>
         </li>
